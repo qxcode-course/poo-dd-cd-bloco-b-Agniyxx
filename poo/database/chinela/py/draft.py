da81d7058a4de2a1bc7c656d151ddd81c955daad
Agne <agne.steph.moss@gmail.com>
2025-10-07 16:54:21 -0300

    configurando o novo repo e iniciando chinela

---
+++ poo/database/chinela/py/draft.py
@@ -0,0 +1,16 @@
+class chinela: 
+    def __init__(self, tamanho):
+        self.tamanho = 0
+    
+    def setTamanho(self):
+        self.tamanho = self.setTamanho
+        if self.tamanho < 20 and self.tamanho > 50:
+            print(f"fail: tamanho atibuido invalido")
+        elif self.tamanho
+        
+    while 
+        
+
+
+
+    
