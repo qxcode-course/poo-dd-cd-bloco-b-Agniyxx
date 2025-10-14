1a998a2d3871515c82cdb984d365d5307beb51cf
Agne <agne.steph.moss@gmail.com>
2025-10-13 23:14:33 -0300

    finalizando chinela e começando camisa

---
+++ poo/database/camisa/py/draft.py
@@ -0,0 +1,8 @@
+class roupa:
+    def __init__(self, tamanhos: str):
+        self.tamanhos: str = " "
+        self.setTamanho(tamanhos)
+
+    def setTamanho(self, tamanho:str):
+            if 
+  
--- poo/database/chinela/py/draft.py
+++ poo/database/chinela/py/draft.py
@@ -1,16 +1,17 @@
 class chinela: 
-    def __init__(self, tamanho):
+    def __init__(self, tamanho: int):
         self.tamanho = 0
     
-    def setTamanho(self):
-        self.tamanho = self.setTamanho
-        if self.tamanho < 20 and self.tamanho > 50:
-            print(f"fail: tamanho atibuido invalido")
-        elif self.tamanho
-        
-    while 
-        
-
-
-
     
+    def setTamanho(self, valor: int):
+        if not (valor, int):
+             print(f"fail:o tamanho deve ser um numero inteiro")
+        if not (20 <= self.tamanho <= 50):
+            print(f"fail: o tamanho atibuido deve ser um numero entre 20 e 50")
+            return
+        if not valor % 2 != 0: 
+             print(f"fail:o tamanho deve ser um numero par")
+             return
+        if self.tamanho == valor:
+            print(f"o tamanho foi definido como {self.tamanho}")
+
