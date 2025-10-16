bd4cd468eba26b25c3f5971aa2a4cceed0df81ef
Agne <agne.steph.moss@gmail.com>
2025-10-16 13:26:53 -0300

    teste tko

--- myrep/poo/chinela/py/draft.py
+++ myrep/poo/chinela/py/draft.py
@@ -5,12 +5,12 @@ class chinela:
     def setTamanho(self, valor: int):
         if not (valor, int):
              print(f"fail:o tamanho deve ser um numero inteiro")
-        if not (20 <= self.tamanho <= 50):
+        if not (20 <= self.__tamanho <= 50):
             print(f"fail: o tamanho atibuido deve ser um numero entre 20 e 50")
             return
         if not valor % 2 != 0: 
              print(f"fail:o tamanho deve ser um numero par")
              return
-        if self.tamanho == valor:
-            print(f"o tamanho foi definido como {self.tamanho}")
+        if self.__tamanho == valor:
+            print(f"o tamanho foi definido como {self.__tamanho}")
 
