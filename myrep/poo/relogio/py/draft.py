f32fdc659da34458feb9534c51eb7efa05c07c1f
Agne <agne.steph.moss@gmail.com>
2025-10-20 16:47:00 -0300

    finalizando relogio e testando

--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -1,24 +1,23 @@
 class Relogio:
     def __init__(self, horas: int, minutos: int, segundos: int):
-        return
-        self.__horas = horas
-        self.__minutos == minutos
-        self.__segundos = segundos
-        self.set_hora(hora)
-        self.set_minuto(minuto)
+        self.__horas = 0
+        self.__minutos = 0
+        self.__segundos = 0
+        self.set_horas(horas)
+        self.set_minutos(minutos)
         self.set_segundos(segundos)
 
-    def set_horas(self, horas: int = 0):
+    def set_horas(self, horas: int):
         if horas < 0 or horas > 23:
             print("fail: hora invalida")
 
     def set_minutos(self, minutos: int):
      if minutos < 0 or minutos > 59:
-            print("fail: hora invalida")
+        print("fail: minuto invalido")
 
     def set_segundos(self, segundos: int):
      if segundos < 0 or segundos > 59:
-            print("fail: hora invalida")
+        print("fail: segundo invalido")
 
     def nextSecond(self):
         if self.__segundos <= 59:
@@ -32,27 +31,28 @@ class Relogio:
         if self.__horas <= 23:
             self.__horas = 0
 
-    def toString(self) -> int:
-        return f"HH: {self.__horas: 02}, MM: {self.__minutos:02}, SS: {self.__segundos:02}")
+    def __str__(self):
+        return f"{self.__horas:02}:{self.__minutos:02}:{self.__segundos:02}"
 
 def main():
-    relogio = Relogio()
+    relogio = Relogio(0,0,0)
     while True:
         line: str = input()
-        print("$ {line}")
+        print(f"${line}")
         args: list[str] = line.split(" ")
         if args[0]== "end":
             break
-        if args[0] == "show":
-            print(relogio.set_horas())
-        if args[0] == "set":
-
-        if args[0] == "next":
-            
-
-
-
-
-
+        elif args[0] == "set":
+            relogio.set_horas(int(args[1]))
+            relogio.set_minutos(int(args[2]))
+            relogio.set_segundos(int(args[3]))
+        elif args[0] == "show":
+            print(relogio)
+        elif args[0] == "next":
+            relogio.nextSecond()
+        elif args[0] == "init":
+            relogio = Relogio(int(args[1]), int(args[2]), int(args[3]))
+        else:
+            print("fail: comando invalido")
 
 main()
