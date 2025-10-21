2de1ba1f17959b838497274f50bac3491e989ec9
Agne <agne.steph.moss@gmail.com>
2025-10-21 16:13:02 -0300

    fazendo motoca

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -1,6 +1,6 @@
 class Pessoa:
     def __init__(self, nome: str):
-        self.nome = nome
+        self.__nome = nome
     def get_nome(self):
         return self.__nome
     def set_nome(self, valor:str):
@@ -9,41 +9,45 @@ class Pessoa:
         return self.__nome
     
 class Moto:
-    def __init__(self):
-        self.cliente: Pessoa | None = None
+    def __init__(self, potencia:int = 1):
+        self.pessoa: Pessoa | None = None
+        self.potencia = potencia
+        self.tempo = 0
 
-    def inserir(self, cliente: Pessoa) -> bool:
-        if self.cliente != None:
+    def inserir(self, pessoa: Pessoa) -> bool:
+        if self.pessoa != None:
             print("moto ocupada")
             return False
-        self.cliente = cliente
+        self.pessoa = pessoa
         return True
 
     def remover(self) -> Pessoa| None:
-        if self.cliente == None:
+        if self.pessoa == None:
             print("moto vazia")
             return None
-        aux: Pessoa = self.cliente
-        self. cliente = None
+        aux: Pessoa = self.pessoa
+        self.pessoa = None
         return aux
     
     def __str__(self) -> str:
-        return f"moto: {self.cliente}"
+        return f"power:{self.valor}, time:{self.tempo}, person:{self.pessoa}"
     
 def main():
+    moto = Moto()
     while True:
-        moto = Moto()
         line:str = input()
         print(f"${line}")
-        args: list
+        args: list[str] = line.split(" ")
+        break
+    if args[0] == "end":
+        break
+    elif args[0]:
+        print("")
+    elif args[0] == "show":
+        print(moto)
+        potencia = int(args[1])
+        
+    elif args[0] == "init":
 
-        if args[0] == "end":
-            break
-        elif args[0]:
-            print("moto")
-        elif args[0] == "show":
-            print("")
-        elif args[0] == "":
-
-        else:
+    else:
             print("fail: comando invalido")
