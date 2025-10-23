ab4bac4bc199f5f35691cebdcbd63f45ca0afdeb
Agne <agne.steph.moss@gmail.com>
2025-10-23 11:41:52 -0300

    fazendo motoca dnv

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -1,27 +1,31 @@
 class Pessoa:
-    def __init__(self, nome: str):
+    def __init__(self, nome: str, idade: int):
         self.__nome = nome
+        self.__idade = idade
+
     def get_nome(self):
         return self.__nome
-    def set_nome(self, valor:str):
-        self.__nome = valor
-    def __str__ (self):
-        return self.__nome
     
+    def get_idade(self):
+        return self.__idade 
+    
+    def __str__ (self):
+        return self.__nome: self.__idade
+  
 class Moto:
     def __init__(self, potencia:int = 1):
         self.pessoa: Pessoa | None = None
         self.potencia = potencia
         self.tempo = 0
 
-    def inserir(self, pessoa: Pessoa) -> bool:
+    def enter(self, pessoa: Pessoa) -> bool:
         if self.pessoa != None:
             print("moto ocupada")
             return False
         self.pessoa = pessoa
         return True
 
-    def remover(self) -> Pessoa| None:
+    def remove(self) -> Pessoa| None:
         if self.pessoa == None:
             print("moto vazia")
             return None
@@ -29,8 +33,35 @@ class Moto:
         self.pessoa = None
         return aux
     
+    def comprarTempo(self, time: int):
+        if time <= 0:
+            print("fail: tempo invalido")
+            return
+        self.tempo += time
+
+    def drive(self, time: int):
+        if self.pessoa == None:
+            print("fail: não ha ninguem na moto")
+            return
+        if self.pessoa.get_idade() < 10:
+            print("fail: muito jovem pra pilotar")
+            return
+        if self.tempo == 0:
+            print("fail: primeiro compre tempo")
+            return
+        if time > self.tempo: 
+            print(f"fail: tempo acabou após andar {self.tempo} minutos")
+        else:
+            self.tempo -= time
+
+    def honk(self):
+        print("P" + "e" * self.potencia "m")
+
+    def show(self):
+        print(self)
+    
     def __str__(self) -> str:
-        return f"power:{self.valor}, time:{self.tempo}, person:{self.pessoa}"
+        return f"power:{self.potencia}, time:{self.tempo}, person:{self.pessoa}"
     
 def main():
     moto = Moto()
@@ -41,13 +72,20 @@ def main():
         break
     if args[0] == "end":
         break
-    elif args[0]:
-        print("")
     elif args[0] == "show":
         print(moto)
-        potencia = int(args[1])
-        
     elif args[0] == "init":
+        
+    elif args[0] == "enter":
+       nome = args[1]
+       idade =
+    elif args[0] == "leave":
 
+    elif args[0] == "buy":
+           
+    elif args[0] == "drive":
+
+    elif args[0] == "honk":
+       
     else:
             print("fail: comando invalido")
