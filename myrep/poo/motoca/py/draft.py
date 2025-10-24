eb959c5ad915ccf9a1a9a1decdc9b0428b3e7e1d
Agne <agne.steph.moss@gmail.com>
2025-10-24 11:40:07 -0300

    fazendo motoca so da erros esses testeeeeees

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -10,7 +10,7 @@ class Pessoa:
         return self.__idade 
     
     def __str__ (self):
-        return self.__nome: self.__idade
+        return f"{self.__nome} : {self.__idade}"
   
 class Moto:
     def __init__(self, potencia:int = 1):
@@ -19,49 +19,46 @@ class Moto:
         self.tempo = 0
 
     def enter(self, pessoa: Pessoa) -> bool:
-        if self.pessoa != None:
-            print("moto ocupada")
+        if self.pessoa is not None:
+            print("fail: busy motorcycle")
             return False
         self.pessoa = pessoa
         return True
 
     def remove(self) -> Pessoa| None:
-        if self.pessoa == None:
-            print("moto vazia")
-            return None
-        aux: Pessoa = self.pessoa
+        if self.pessoa is None:
+            print("fail: empty motorcycle")
+            return
+        print(f"{self.pessoa}")
         self.pessoa = None
-        return aux
     
     def comprarTempo(self, time: int):
         if time <= 0:
-            print("fail: tempo invalido")
+            print("fail: buy time first")
             return
         self.tempo += time
 
     def drive(self, time: int):
-        if self.pessoa == None:
-            print("fail: não ha ninguem na moto")
+        if self.pessoa is None:
+            print("fail: buy time first")
             return
         if self.pessoa.get_idade() < 10:
-            print("fail: muito jovem pra pilotar")
-            return
-        if self.tempo == 0:
-            print("fail: primeiro compre tempo")
+            print("fail: too old to drive")
             return
         if time > self.tempo: 
-            print(f"fail: tempo acabou após andar {self.tempo} minutos")
+            print(f"fail: time finished after {self.tempo} minutes")
         else:
             self.tempo -= time
 
     def honk(self):
-        print("P" + "e" * self.potencia "m")
+        print("P" + "e" * self.potencia + "m")
 
     def show(self):
         print(self)
     
     def __str__(self) -> str:
-        return f"power:{self.potencia}, time:{self.tempo}, person:{self.pessoa}"
+        str_pessoa = str(self.pessoa) if self.pessoa else "empty"
+        return f"power:{self.potencia}, time:{self.tempo}, person:({str_pessoa})"
     
 def main():
     moto = Moto()
@@ -69,23 +66,27 @@ def main():
         line:str = input()
         print(f"${line}")
         args: list[str] = line.split(" ")
-        break
-    if args[0] == "end":
-        break
-    elif args[0] == "show":
-        print(moto)
-    elif args[0] == "init":
         
-    elif args[0] == "enter":
-       nome = args[1]
-       idade =
-    elif args[0] == "leave":
-
-    elif args[0] == "buy":
-           
-    elif args[0] == "drive":
-
-    elif args[0] == "honk":
-       
-    else:
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(moto)
+        elif args[0] == "init":
+            moto = Moto(int(args[1]))
+        elif args[0] == "enter":
+            nome = args[1]
+            idade = int(args[2])
+            pessoa = Pessoa(nome, idade)
+            moto.enter(pessoa)
+        elif args[0] == "leave":
+            moto.remove()
+        elif args[0] == "buy":
+            moto.comprarTempo(0)
+        elif args[0] == "drive":
+            moto.drive(0)
+        elif args[0] == "honk":
+            moto.honk()
+        else:
             print("fail: comando invalido")
+
+main()
